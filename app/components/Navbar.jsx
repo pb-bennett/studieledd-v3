a5e04f77f6e33c1877d39ae29e7a837d90d75a64
pb-bennett <97233485+pb-bennett@users.noreply.github.com>
2023-10-29 20:52:38 +0100

    next-auth working.  now for logout

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,20 +1,30 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { useSession, signOut } from 'next-auth/react';
 
 import Logo from './sl-logo.svg';
 
-const Navbar = () => {
+export default function Navbar() {
+  const { data, status } = useSession();
+  console.log('data:', data, 'status:', status);
   return (
     <div className="px-8">
       <nav>
         <Image src={Logo} alt="StudieLedd logo" height={60} quality={100} />
         <h1>StudieLedd</h1>
         <Link href="/">Home</Link>
-        <Link href="/login">Login</Link>
-        <Link href="/register">Register</Link>
+        {status === 'authenticated' ? (
+          <div className="flex gap-1">
+            <Link href="/dashboard">Dashboard</Link>
+            <Link href="/">Logout</Link>
+          </div>
+        ) : (
+          <div className="flex gap-1">
+            <Link href="/login">Login</Link>
+            <Link href="/register">Register</Link>
+          </div>
+        )}
       </nav>
     </div>
   );
-};
-
-export default Navbar;
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,28 @@
+'use client';
 import './globals.css';
 import { Rubik } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
 
 // Components
 import Navbar from './components/Navbar';
 
 const rubik = Rubik({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'StudieLedd',
-  description: 'Connecting dremote based students.',
-};
+// export const metadata = {
+//   title: 'StudieLedd',
+//   description: 'Connecting dremote based students.',
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={rubik.className}>
-        <Toaster />
-        <Navbar />
-        {children}
+        <SessionProvider>
+          <Toaster />
+          <Navbar />
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
